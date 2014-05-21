7f8762ce28729be7d8b8130197451b44cec7bda3
cmyr <colin@cmyr.net>
2014-05-21 15:56:57 -0400

    added main view controller

---
+++ iKu/classes/IKUMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  IKUMainViewController.h
+//  iKu
+//
+//  Created by Colin Rofls on 2014-05-21.
+//  Copyright (c) 2014 Brisk Synergies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IKUMainViewController : UIViewController
+
+@end
---
+++ iKu/classes/IKUMainViewController.m
@@ -0,0 +1,54 @@
+//
+//  IKUMainViewController.m
+//  iKu
+//
+//  Created by Colin Rofls on 2014-05-21.
+//  Copyright (c) 2014 Brisk Synergies. All rights reserved.
+//
+
+#import "IKUMainViewController.h"
+#import "IKUHaikuSource.h"
+#import "IKUHaiku.h"
+
+@interface IKUMainViewController ()
+@property (strong, nonatomic) IKUHaikuSource* haikuSource;
+@end
+
+@implementation IKUMainViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    self.haikuSource = [IKUHaikuSource sharedInstance];
+    [self.haikuSource _setupDebugData];
+    
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
