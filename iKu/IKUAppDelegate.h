5759b7a31aa90529a5c510c49ce77becf7fda330
cmyr <colin@cmyr.net>
2014-05-21 14:36:27 -0400

    Initial Commit

---
+++ iKu/IKUAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  IKUAppDelegate.h
+//  iKu
+//
+//  Created by Colin Rofls on 2014-05-21.
+//  Copyright (c) 2014 Brisk Synergies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IKUAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
